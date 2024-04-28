a7e5c5a8f801dd0ebdcaf6f0ec12847d1b536ba4
Misael Sabbadini <sabbadini.misael@gmail.com>
2024-04-28 17:48:29 +0200

    chore: add maturin cmd to pipeline script

--- build-exe.ps1
+++ build-exe.ps1
@@ -2,5 +2,6 @@ py -3.9 -m venv .venv-fcs
 .venv-fcs\Scripts\Activate.ps1
 pip install pyinstaller
 pip install -r .\requirements.txt
+maturin develop
 pyinstaller --noconsole --onefile --icon .\assets\fcs-logo.png --add-data "assets/*:assets"  .\fcs.py
 deactivate
