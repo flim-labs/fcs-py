9306ee0777aeae0089472a32be76a1f6f74d04e4
Misael Sabbadini <sabbadini.misael@gmail.com>
2026-01-26 19:04:55 +0100

    fxi: update build-exe.ps1 to use python command for virtual environment and package installations

--- build-exe.ps1
+++ build-exe.ps1
@@ -1,7 +1,8 @@
-py -3.9 -m venv .venv-fcs
-.venv-fcs\Scripts\Activate.ps1
-pip install pyinstaller
-pip install -r .\requirements.txt
-pip install PyQt6 --force-reinstall
+python -m venv .venv-fcs
+.\.venv-fcs\Scripts\Activate.ps1
+python -m pip install --upgrade pip
+python -m pip install pyinstaller
+python -m pip install -r .\requirements.txt
+python -m pip install PyQt6 --force-reinstall
 pyinstaller --onefile --icon .\assets\fcs-logo.ico --add-data "assets/*:assets" --add-data "export_data_scripts/*:export_data_scripts" --hidden-import=matplotlib.backends.backend_ps --hidden-import=matplotlib.backends.backend_agg .\fcs.py
 deactivate
